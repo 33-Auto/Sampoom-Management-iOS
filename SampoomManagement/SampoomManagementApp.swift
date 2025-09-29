162eb3afdc01308ec6835715b7fca927f2e77142
Sangyoon <qlrqod123123@naver.com>
2025-09-29 13:57:31 +0900

    initial commit

---
+++ SampoomManagement/SampoomManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampoomManagementApp.swift
+//  SampoomManagement
+//
+//  Created by 채상윤 on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct SampoomManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
